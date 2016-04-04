dc7ea5d882daeb7c367b9006cbb88367348fac7c
Nastya Redko <redkoanastasia03@gmail.com>
2016-04-04 22:13:54 +0300

    разработка цикла

---
+++ csharp.lab1.sen2/csharp.lab1.sen2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ForbiddenWords
+{
+    class ForbiddenWords
+    {
+        static void Main()
+        {
+            
+
+            while (index <= text.Length - 1)
+            {
+                for (int i = 0; i < Words.Length; i++)
+                {
+                    string word = Words[i].ToUpper();
+                    if (text.IndexOf(Words[i], index, StringComparison.InvariantCultureIgnoreCase) != -1)
+                    {
+                        Regex.Replace(text, Words[i], new string('*', Words[i].Length), Words.IgnoreCase);
+                    }
+                }
+                index++;
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ csharp.lab1.sen2/csharp.lab1.sen2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("csharp.lab1.sen2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("csharp.lab1.sen2")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ededd6a2-49e6-4ef4-a50d-63d630105108")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
