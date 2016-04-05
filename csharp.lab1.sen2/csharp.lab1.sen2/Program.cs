e64f493d341484363e8bde909bcf85c62ec588f5
Sasha Malyuk <malyuk.sandra@gmail.com>
2016-04-05 23:05:14 +0300

    0

--- csharp.lab1.sen2/csharp.lab1.sen2/Program.cs
+++ csharp.lab1.sen2/csharp.lab1.sen2/Program.cs
@@ -5,13 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
+
 namespace ForbiddenWords
 {
     class ForbiddenWords
     {
         static void Main()
         {
-            
+            string text = "Microsoft announced its next generation PHP compiler today. It is based on .NET Framework 4.0 and is implemented as a dynamic language in CLR.";
+            Console.WriteLine(text);
+            int index = 0;
+                
+            string[] Words = { "PHP", "CLR", "Microsoft" };
 
             while (index <= text.Length - 1)
             {
@@ -20,12 +25,14 @@ namespace ForbiddenWords
                     string word = Words[i].ToUpper();
                     if (text.IndexOf(Words[i], index, StringComparison.InvariantCultureIgnoreCase) != -1)
                     {
-                        Regex.Replace(text, Words[i], new string('*', Words[i].Length), Words.IgnoreCase);
+                        Regex.Replace(text, Words[i], new string('*', Words.Length), RegexOptions.IgnoreCase);
                     }
                 }
                 index++;
             }
             Console.WriteLine(text);
+            Console.ReadLine();
         }
-    }
+        
+    } 
 }
